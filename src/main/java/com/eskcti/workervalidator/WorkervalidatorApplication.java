e66f69e4adaaca6089a8d0da4d6b20a7b105ec3f
eskokado <eskokado@gmail.com>
2023-11-25 14:23:42 -0300

    first commit

---
+++ src/main/java/com/eskcti/workervalidator/WorkervalidatorApplication.java
@@ -0,0 +1,13 @@
+package com.eskcti.workervalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkervalidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkervalidatorApplication.class, args);
+	}
+
+}
